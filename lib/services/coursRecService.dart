615348d51386525bad9271b654e4adbd5fb0f21d
23karim <karim.kekli@esprit.tn>
2024-04-20 18:19:47 +0200

    serviceCours

---
+++ lib/services/coursRecService.dart
@@ -0,0 +1,31 @@
+// services/cours_service.dart
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:pim/models/CoursR.dart';
+
+class CoursRecService {
+  static Future<List<CoursR>> fetchCours() async {
+    final response = await http.get(Uri.parse('http://localhost:9090/cours/rec'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonResponse = json.decode(response.body);
+      return jsonResponse.map((item) {
+        return CoursR(
+          nomCoursR: item['nomCoursR'],
+          description: item['description'],
+          pdff: item['pdff'],
+        );
+      }).toList();
+    } else {
+      throw Exception('Échec de la récupération des cours');
+    }
+  }
+/*
+  static Future<void> deleteCours(String id) async {
+    final response = await http.delete(Uri.parse('http://localhost:9090/cours/recid/$id'));
+
+    if (response.statusCode != 200) {
+      throw Exception('Échec de la suppression du cours');
+    }
+  }*/
+}
