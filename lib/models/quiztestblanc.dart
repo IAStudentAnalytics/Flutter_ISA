9945a93726add6768be439cfdeb54008e22640a1
abir <abir.belkaroui@gmail.com>
2024-04-20 13:25:31 +0200

    model pour le quiz de test blanc

---
+++ lib/models/quiztestblanc.dart
@@ -0,0 +1,13 @@
+class QuizQuestion {
+  final String question;
+  final List<String> answers;
+  final String correctAnswer;
+  final String chapter;
+
+  QuizQuestion({
+    required this.question,
+    required this.answers,
+    required this.correctAnswer,
+    required this.chapter,
+  });
+}
