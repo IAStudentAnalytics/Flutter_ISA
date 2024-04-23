de344a80d2dbab32d0c22398d343e1bd3f4c11f3
abir <abir.belkaroui@gmail.com>
2024-04-23 15:23:24 +0200

    ajouter interface de recommandation des lien youtube

--- lib/services/compilateurService.dart
+++ lib/services/compilateurService.dart
@@ -19,7 +19,7 @@ class CompilerService {
     }
   }
    Future<Map<String, dynamic>> fetchRecommendedVideos(String testId, String studentId) async {
-    final response = await http.get(Uri.parse('http://127.0.0.1:60430/note/rec/$testId/$studentId'));
+    final response = await http.get(Uri.parse('${APIConstants.baseURL}/note/rec/$testId/$studentId'));
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
